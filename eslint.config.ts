e1c2d1cdcf69f6c88c0ae2b62d4abc8c17132fa9
romaleev <aromaleev@gmail.com>
2025-10-07 17:48:00 +0300

    configs fix

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,23 +3,28 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, eslintConfigPrettier, {
-	files: ['**/*.ts'],
-	ignores: ['*.d.ts', '*.js'],
-	languageOptions: {
-		parserOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-			projectService: true,
-			tsconfigRootDir: import.meta.dirname,
+export default [
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	eslintConfigPrettier,
+	{
+		files: ['**/*.ts'],
+		ignores: ['*.d.ts', '*.js'],
+		languageOptions: {
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+			globals: {
+				...globals.node,
+			},
 		},
-		globals: {
-			...globals.node,
+		rules: {
+			'require-await': 'error',
+			'@typescript-eslint/no-floating-promises': 'error',
+			'@typescript-eslint/no-deprecated': 'error',
 		},
 	},
-	rules: {
-		'require-await': 'error',
-		'@typescript-eslint/no-floating-promises': 'error',
-		'@typescript-eslint/no-deprecated': 'error',
-	},
-})
+]
